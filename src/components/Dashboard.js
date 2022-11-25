2b6398646185f0fcdd4fd79991a318c418459cca
Sszyh <zhuyer@outlook.com>
2022-11-24 17:41:00 -0800

    add instance method to Dashboard and pass props to Panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -27,24 +27,36 @@ const data = [
 ];
 
 
-
 class Dashboard extends Component {
-  state = { loading: false};
- 
+  state = {
+    loading: false,
+    focused: null
+  };
+
+  selectPanel(id) {
+
+    this.setState((previousState) => ({ focused: previousState.focused ? null : id }));//?
+
+  }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map((panel) => (
-      <Panel
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-      />
-    ));
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={event => this.selectPanel(panel.id)}
+        />
+      ));
 
     return <main className={dashboardClasses}>{panels}</main>;
   }
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,12 +2,10 @@ import React, { Component } from "react";
 
 class Panel extends Component {
   render() {
-    const { label, value } = this.props;
+    const { label, value, onSelect } = this.props;
 
     return (
-      <section
-        className="dashboard__panel"
-      >
+      <section className="dashboard__panel" onClick={onSelect}>
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
       </section>
